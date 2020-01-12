6f5b049c9db531d42a1b4e7be6f0b28dd03f0df5
David <dngames2006@gmail.com>
2020-01-11 21:26:10 -0600

    Add project files.

---
+++ src/mem/mem.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <windows.h>
+#include <vector>
+
+//https://guidedhacking.com/threads/how-to-hack-any-game-first-internal-hack-dll-tutorial.12142/
+
+namespace mem
+{
+	void Patch(BYTE* dst, BYTE* src, unsigned int size);
+	void Nop(BYTE* dst, unsigned int size);
+	uintptr_t FindDMAAddy(uintptr_t ptr, std::vector<unsigned int> offsets);
+}
